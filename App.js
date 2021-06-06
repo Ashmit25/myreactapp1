fba039954cb67607c5009e1fdab862756fbc349a
Ashmit25 <83855143+Ashmit25@users.noreply.github.com>
2021-06-06 20:11:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  };
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Increment/Decrement app</h1>
+        <br></br>
+        <button onClick={this.IncrementItem} >
+          +
+        </button>
+        {this.state.show ? <h2>{this.state.clicks}</h2> : ""}
+        <button onClick={this.DecreaseItem}>-</button>
+      </div>
+    );
+  }
+}
+export default App;
